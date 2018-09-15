d845c578585e52b96493183fe7a9af6774c1cef5
jasonmziegler <jason@insightfulautomation.com>
2018-09-14 22:44:05 -0500

    add publisher route and html

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -7,3 +7,11 @@ from flask import render_template
 def hello():
     books = Book.query.all()
     return render_template('home.html', books=books)
+
+@main.route('/display/publisher/<publisher_id>')
+def display_publisher(publisher_id):
+    publisher = Publication.query.filter_by(id=publisher_id).first()
+
+    publisher_books = Book.query.filter_by(pub_id = publisher.id).all()
+
+    return render_template('publisher.html', publisher=publisher, publisher_books=publisher_books)
