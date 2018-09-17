9f4e6062578eb91c97d9151b0e7d46259ad599c1
jasonmziegler <jason@insightfulautomation.com>
2018-09-17 00:38:00 -0500

    add create and edit

--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -3,6 +3,7 @@ from app import db
 from app.catalog.models import  Book, Publication
 from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_required
+from app.catalog.forms import EditBookForm, CreateBookForm
 
 @main.route('/')
 def hello():
@@ -28,3 +29,39 @@ def delete_book(book_id):
         flash('Book deleted successfully')
         return redirect(url_for('main.hello'))
     return render_template('delete_book.html', book=book, book_id=book.id)
+
+@main.route('/book/edit/<book_id>', methods=['GET', 'POST'])
+@login_required
+def edit_book(book_id):
+    book = Book.query.get(book_id)
+    form = EditBookForm(obj=book)
+    if form.validate_on_submit():
+        book.title = form.title.data
+        book.format = form.format.data
+        book.num_pages = form.num_pages.data
+        db.session.add(book)
+        db.session.commit()
+        flash('Book Edited Successfully')
+        return redirect(url_for('main.hello'))
+    return render_template('edit_book.html', form=form)
+
+@main.route('/book/create/<pub_id>', methods=['GET','POST'])
+@login_required
+def create_book(pub_id):
+    form = CreateBookForm()
+    form.pub_id.data = pub_id
+    if form.validate_on_submit():
+        book=Book(
+            title=form.title.data,
+            author=form.author.data,
+            avg_rating=form.avg_rating.data,
+            book_format=form.format.data,
+            image=form.img_url.data,
+            num_pages=form.num_pages.data,
+            pub_id=form.pub_id.data
+        )
+        db.session.add(book)
+        db.session.commit()
+        flash('Book added successfully')
+        return redirect(url_for('main.display_publisher', publisher_id=pub_id))
+    return render_template('create_book.html', form=form, pub_id=pub_id)
